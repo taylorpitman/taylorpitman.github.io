f57e9b241020dd6405d8f2e2762f0f0c84bd008e
taylorpitman <123507995+taylorpitman@users.noreply.github.com>
2025-08-16 15:35:13 -0400

    Added CTA Buttons and Status
    Added a contact button and availability status

--- portfolio/src/components/Contact/contactCard.jsx
+++ portfolio/src/components/Contact/contactCard.jsx
@@ -1,31 +1,24 @@
-import headshot from '../../assets/headshots/headshot.jpg';
-
 
 const ContactCard = () => {
 
     return (
-        <div className='max-w-2xl mt-20 mb-20 w-full mx-auto items-center justify-center flex px-8 md:px-6 gap-6 rounded-xl shadow-lg bg-white text-zinc-900'>
-            <div className='flex-col p-4'>
-                {/*Profile Picture */}
-                <div>
-                    <img src={headshot} alt="Profile" className="w-16 rounded-full shadow-md" />
-                </div>
+        <div className='max-w-2xl mt-20 mb-20 w-full mx-auto flex flex-col gap-3 px-8 md:px-6 text-zinc-900'>
+                <h1 className='text-2xl font-bold'>Hire Me</h1>
+                <p className='text-md text-zinc-600'>Need an end-to-end builder who’s flexible, fast, and passionate about craft? I turn ideas into responsive, production-ready experiences and love collaborating to iterate. Reach out!</p>
 
-                {/*Title & Contact tagline */}
-                <div className='flex flex-col items-center w-2/3 mt-4 text-center'>
-                    <h2 className='text-2xl font-bold'>Hire me.</h2>
-                    <p className='text-sm text-zinc-600'>Front-end Developer</p>
-                    <div>
-                        <p className='text-sm text-zinc-600'>Email: taylor@example.com</p>
-                        <p className='text-sm text-zinc-600'>LinkedIn: linkedin.com/in/taylor</p>
+                {/* CTA Button, and Open to opportunities */}
+                <div className='flex items-center gap-4 '>
+                    <button className="inline-flex justify-center items-center gap-2.5 font-medium transition-colors duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-offset-2 bg-zinc-900  border-zinc-900 border-2 text-white hover:text-zinc-900 hover:bg-zinc-50 focus:ring-zinc-500 px-6 py-2 text-sm rounded-xl  cursor-pointer">
+                    Contact Me</button>
+
+                    <div className='flex items-center bg-emerald-100 rounded-3xl p-4 gap-2'>
+                        <span class="relative h-2.5 w-2.5 flex items-center" aria-hidden="true">
+                            <span class="motion-safe:absolute motion-safe:inline-flex h-full w-full rounded-full bg-emerald-400 opacity-75 animate-ping"></span>
+                            <span class="relative inline-flex h-2.5 w-2.5 rounded-full bg-emerald-600 ring-2 ring-emerald-200"></span>
+                        </span>
+                        <p className=' text-sm text-zinc-600'>Open to opportunities</p>
                     </div>
                 </div>
-
-                {/*open to opportunities */}
-                <div className='text-sm text-zinc-600'>
-                    <p>Open to new opportunities</p>
-                </div>
-            </div>
         </div>
 
     )
